66325884f3f0009fa0bd9e00c9102ba189be2d67
fabian <fabian.makt@gmail.com>
2019-01-07 21:51:54 +0100

    ejr2

---
+++ ejr2.rb
@@ -0,0 +1,15 @@
+class MiClase
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+
+  def saludar
+    "Hola! Soy la clase #{@name}"
+  end
+end
+
+c = MiClase.new('Clase Uno')
+# puts c.name
+c.name = 'Miclase'
+puts c.saludar
